4cf6834cadab4bb1a217c9415d263d960934e9da
Pål-Christian By <Pal.christian.by@gmail.com>
2020-02-18 19:43:52 +0100

    Updates

---
+++ precache-manifest.c945a6f881efe68a19be7ab68c34f619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb934c373a0d5d8d35f4315c099de6a9",
+    "url": "/github-allstars/index.html"
+  },
+  {
+    "revision": "488a43efec5f067d896d",
+    "url": "/github-allstars/static/css/main.45112721.chunk.css"
+  },
+  {
+    "revision": "b60882e27f5e6cbd2baa",
+    "url": "/github-allstars/static/js/2.8d13677b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/github-allstars/static/js/2.8d13677b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488a43efec5f067d896d",
+    "url": "/github-allstars/static/js/main.cbe89126.chunk.js"
+  },
+  {
+    "revision": "846ef55b7896cb522267",
+    "url": "/github-allstars/static/js/runtime-main.f4007373.js"
+  }
+]);
